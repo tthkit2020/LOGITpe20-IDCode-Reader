5cbb83ddb6056a25b230ddae22cb239efb886351
Julia Aasmaa <my_e_10@hotmail.com>
2020-10-19 15:19:27 +0300

    add getAge

---
+++ YearValidator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace YearValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth:");
+            string userInput = Console.ReadLine();
+
+            if(Validate(userInput))
+            {
+
+            }
+        }
+
+        public static bool Validate(string yearInput)
+        {
+            if(yearInput.Length == 4)
+            {
+                try
+                {
+                    Int32.Parse(yearInput);
+                    Console.WriteLine("All good.");
+                    return true;
+                } catch(Exception error)
+                {
+                    Console.WriteLine(error);
+                    return false;
+                }
+            } else
+            {
+                Console.WriteLine("Wrong format.");
+                return false;
+            }
+        }
+
+
+    }
+}
